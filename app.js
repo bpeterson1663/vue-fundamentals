000b7a897459a490579898cf33e1902d1ab1f7c4
Brady Peterson <bpeterson1663@gmail.com>
2021-06-10 21:28:04 -0500

    feat(methods): use methods to display complex logic

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({
+    data() {
+        return {
+            greeting: 'Hello',
+            firstName: 'Brady'
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.greeting} ${this.firstName}`
+        }
+    }
+}).mount('#app')
+
+// setTimeout(() => {
+//     //Proxy setter and getter. Build into Vue
+//     vm.firstName = 'Bob'
+// }, 2000)
