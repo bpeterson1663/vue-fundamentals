b4f9bb18ef5f9c45c709fed5d155830967941279
Brady Peterson <bpeterson1663@gmail.com>
2021-06-12 15:32:04 -0500

    feat(lifecycle hooks): working with lifecycle hooks

---
+++ mounting-vue-instance/app.js
@@ -0,0 +1,37 @@
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate called", this.message)
+  },
+  created(){
+    console.log("created called", this.message)
+  },
+  beforeMount(){
+    console.log("beforeMount called", this.$el)
+  },
+  mounted() {
+    console.log("beforeMount called", this.$el)
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate called")
+  },
+  updated() {
+    console.log("update")
+  },
+  beforeUnmount(){
+    console.log("beforeUnmount called")
+  },
+  unmounted() {
+    console.log("unmounted called")
+  }
+})
+
+vm.mount('#app')
+
+// setTimeout(() => {
+//   vm.mount('#app')
+// }, 3000)
