0ad10e05c901886f8795fb1e5534afa420577160
Brady Peterson <bpeterson1663@gmail.com>
2021-06-14 22:02:15 -0500

    feat(props): working with props

--- vue-cli-demo/src/App.vue
+++ vue-cli-demo/src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <p>Hey</p>
-  <Greeting />
+  <h3>Hey</h3>
+  <greeting />
+  <user :age="age"/>
 </template>
 
 <script>
 import Greeting from "./components/Greeting.vue"
+import User from "./components/User.vue"
 export default {
   name: 'App',
   components: {
-    Greeting
+    Greeting,
+    User
+  },
+  data() {
+    return {
+      age: 32
+    }
   }
 }
 </script>
---
+++ vue-cli-demo/src/components/User.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>The user is {{age}} old</p>
+</template>
+
+<script>
+export default {
+    name: "User",
+    props: ["age"]
+}
+</script>
