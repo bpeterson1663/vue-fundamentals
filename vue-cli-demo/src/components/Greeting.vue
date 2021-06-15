b7a4a49354f175f096f3b48bf8090a75abfa8299
Brady Peterson <bpeterson1663@gmail.com>
2021-06-14 22:16:37 -0500

    feat(props): working with prop validations

--- vue-cli-demo/src/components/Greeting.vue
+++ vue-cli-demo/src/components/Greeting.vue
@@ -1,10 +1,17 @@
 <template>
-  <p>{{ msg }}</p>
+  <p v-if="age > 25">{{ msg }}</p>
+  <p v-else>You must be 25 years or older to veiw this</p>
 </template>
 
 <script>
 export default {
   name: 'Greeting',
+  props: {
+      age: {
+          type: Number,
+          required: true
+      }
+  },
   data() {
     return {
       msg: 'Hello World!',
