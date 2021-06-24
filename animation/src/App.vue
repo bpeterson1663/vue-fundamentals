2615ff5cdd54040152e139c56c2d72a74ca6688d
Brady Peterson <bpeterson1663@gmail.com>
2021-06-23 22:38:09 -0500

    feat(transition): conditionally rendering content with transitions

--- animation/src/App.vue
+++ animation/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade">
-    <h2 v-if="flag">Hello World</h2>
-
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello World</h2>
+    <h2 v-else key="secondary">Another Flag</h2>
   </transition>
 </template>
 
